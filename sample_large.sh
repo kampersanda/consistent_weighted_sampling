730d52707898e09f3b22fff1993aef2595ba2015
kampersanda <shnsk.knd@gmail.com>
2018-12-06 16:10:21 +0900

    change bit size

--- sample_large.sh
+++ sample_large.sh
@@ -23,12 +23,12 @@ echo -e "\n#### Step 5. Generate random data. ####"
 ./gen_random_data -r covtype.scale01.rnd -m 54 -s 32
 
 echo -e "\n#### Step 6. Run sampling for the split data ####"
-./cws_large -d data-list.txt -r covtype.scale01.rnd -m 54 -s 32 -b 4 -i 1 -w 1 -l 1 -t 4
+./cws_large -d data-list.txt -r covtype.scale01.rnd -m 54 -s 32 -b 2 -i 1 -w 1 -l 1 -t 4
 
 echo -e "\n#### Step 7. Concatenate the sub sampled data. ####"
 find $PWD/split-data/*.cws > cws-list.txt
-./concatenate -d cws-list.txt -o covtype.scale01.32.4.cws
+./concatenate -d cws-list.txt -o covtype.scale01.32.2.cws
 
-echo -e "\nGood job!! You obtained the sampled dataset covtype.scale01.32.4.cws as follows:"
-head covtype.scale01.32.4.cws
+echo -e "\nGood job!! You obtained the sampled dataset covtype.scale01.32.2.cws as follows:"
+head covtype.scale01.32.2.cws
 echo "..."
